8410282d19e864d9a3a9a8f6571a802d76cafe5c
Silvia Jerez <silviajerez1208@gmail.com>
2025-02-24 18:05:36 -0500

    cambio de funcion

---
+++ js/cambiarimg.js
@@ -0,0 +1,17 @@
+function cambiarImagen() {
+  //lectura de la imagen inicial
+  let imagen = document.getElementById("miImagen");
+  // en un if pregunto que estado tiene si esta en la imagen 1 "perros" cambio a gatos
+  if (imagen.src.includes("/img/faru.png")) {
+    imagen.src = "/img/lyla.png";
+    imagen.style.borderRadius = "20px";
+  } else {
+    imagen.src = "/img/faru.png";
+  }
+}
+
+function cambiarImagen2() {
+  let selector = document.getElementById("selectorImagen");
+  let imagen = document.getElementById("miImagen2");
+  imagen.src = selector.value;
+}
